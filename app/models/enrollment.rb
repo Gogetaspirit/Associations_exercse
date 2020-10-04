dfa9b7bf0ca3a0a91b2cee5465f652576eca2658
ChrisLee <christopher.dan.lee96@gmail.com>
2020-10-04 17:50:20 -0400

    first draft

---
+++ app/models/enrollment.rb
@@ -0,0 +1,24 @@
+# t.integer "course_id"
+# t.integer "student_id"
+# t.datetime "created_at", null: false
+# t.datetime "updated_at", null: false
+
+
+
+class Enrollment < ApplicationRecord
+    belongs_to(
+    :user,
+    class_name: 'User',
+    foreign_key: :student_id,
+    primary_key: :id
+    )
+    
+    
+    belongs_to(
+    :course,
+    class_name: 'Course',
+    foreign_key: :course_id,
+    primary_key: :id
+    )
+
+end
